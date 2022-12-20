ad660b70e271cbf010baec5bee7920ba392b1168
GaGiiiii <dragoslav.gagi8@gmail.com>
2022-12-20 01:29:36 +0100

    Added comments about cache system

--- app/services/statistics/GoogleStatistics.php
+++ app/services/statistics/GoogleStatistics.php
@@ -2,6 +2,20 @@
 
 namespace App\Services\Statistics;
 
+/**
+ * ========= IMPORTANT =========
+ *
+ * We should implement a small cache system here.
+ * Instead of calling positive guy's API every time we should check when was the last call made.
+ * If the last call was made in the last 5 minutes we don't need to call positive guy API again.
+ * Instead we should take the data from the database cache to save some time.
+ * If the last call was made more than 5 minutes ago we should then call positive guy API, get
+ * Their response and save that response in database cache.
+ *
+ * Database table should contain TIMESTAMP of the last call to the API as well as the name of the SERVICE
+ * And the TYPE (day, week, month, year, period). In case of the period we should always call google API.
+ */
+
 class GoogleStatistics implements IStatisticsSource
 {
     /**
